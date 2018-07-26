206ad3da810e12385204b86b863687fc5615a5a0
bradleyeckert <41526070+bradleyeckert@users.noreply.github.com>
2018-07-26 11:09:59 -0700

    Add files via upload

---
+++ Tiff/src/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "vm.h"
+#include "tiff.h"
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    int i;  char *s;  char c;
+    char argline[MaxTIBsize+1];		    // command line string: args separated by blanks
+    int argidx;						    // the string and length
+
+#ifdef __linux__
+    initscr();
+    cbreak();
+    noecho();
+    scrollok(stdscr, TRUE);
+    nodelay(stdscr, TRUE);
+#endif
+
+// The command line string is built from the argvs. You can put quotes around an
+// argument to keep its blanks, but then you will lose the quotes.
+// Here, characters 147 and 148 (open and close quotes) of the ANSI code page
+// 1252 are translated to ASCII quotes.
+
+    argline[0] = 0;                     // default is empty line
+    argidx = 0;							// capture commandline string:
+    if (argc) {
+        for(i=1; i<argc; i++) {
+            s = argv[i];
+            while ((argidx < MaxTIBsize) && (*s)) {
+                c = *s++;
+                if ((c==147) || (c==148))
+                    c = '"';			// replace fancy quotes with plain quotes.
+                argline[argidx++] = c;	// safely copy argument to string
+            }
+            if ((argidx < MaxTIBsize) && (i < (argc-1))) {
+                argline[argidx++] =' ';	// separate by blanks
+            }
+            argline[argidx] = 0;		// zero-terminated string
+        }
+    }
+    tiffQUIT(argline);
+    return 0;
+}
