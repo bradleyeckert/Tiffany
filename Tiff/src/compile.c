5d2f4b08cf656af327aa039b34321b63f7e7ca47
bradleyeckert <41526070+bradleyeckert@users.noreply.github.com>
2018-08-01 22:12:35 -0700

    Add files via upload

--- Tiff/src/compile.c
+++ Tiff/src/compile.c
@@ -5,20 +5,72 @@
 #include "vmaccess.h"
 #include <string.h>
 
-void Literal (uint32_t N) {
-    printf("LIT:%d", N);
+uint32_t IR;
+int slot;
+
+void InitIR (void) {
+    IR = 0;  slot = 26;
+}
+
+// Finish off IR with the largest literal that will fit.
+// All literals are unsigned.
+
+void Explicit (int opcode, uint32_t n) {
+    if (n >= (1<<slot)) {               // it doesn't fit
+        CommaC(IR);
+        InitIR();
+    }
+    IR |= (opcode << slot) | n;
+    CommaC(IR);
+    InitIR();
+}
+
+// Append a zero-operand opcode
+
+void Implicit (int opcode) {
+    if (opcode >= (1<<slot)) {          // doesn't fit in last slot
+        CommaC(IR);
+        InitIR();
+    }
+    IR |= opcode << slot;
+    slot -= 6;
+    if (slot<0) {                       // slots used up
+        CommaC(IR);
+        InitIR();
+    }
+}
+
+void Literal (int32_t N) {
+    uint32_t u = abs(N);
+    if (u < 0x03FFFFFF) {               // single width literal
+        Explicit(opLIT, u);
+        if (N < 0) {
+            Implicit(opCOM);
+        }
+    } else {
+        printf("Lit32:%X ", u);
+        Explicit(opLIT, (N>>24));       // split into 8 and 24
+        Explicit(opShift24, (N & 0xFFFFFF));
+    }
+}
+
+void FakeIt (int opcode) {
+    DbgGroup(opcode, opSKIP, opNOOP, opNOOP, opNOOP);
 }
 
 // Functions invoked after being found, from either xte or xtc.
 // Positive means execute in the VM.
 // Negative means execute in C.
 
-void tiffFUNC (int n, uint32_t W) {
+void tiffFUNC (int n, uint32_t ht) {
     if (n & 0x800000) n |= 0xFF000000; // sign extend 24-bit n
 	if (n<0) { // internal C function
+        uint32_t w = FetchCell(ht-20);
 		switch(~n) {
-			case 0: PushNum(W);  break;
-			case 1: Literal(W);  break;
+			case 0: PushNum (w);  break;
+			case 1: Literal (w);  break;
+			case 2: FakeIt  (w);  break;  // execute opcode
+			case 3: Implicit(w);  break;  // compile implicit opcode
 			default: break;
 		}
 	} else { // execute in the VM
